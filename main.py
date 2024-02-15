ab444186de9f716f94150b3d1656611bc0ee8702
sui-tyan <mallari23adrian@gmail.com>
2024-02-15 11:37:15 +0800

    Update: Added requirements.txt

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import threading
 import cv2
 from deepface import DeepFace
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+# cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
